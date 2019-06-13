81407a47d48cee01231b3cfc8204cab426f74172
Amin Amos <aminamosahs@gmail.com>
2019-06-13 04:18:18 +0000

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,11 +1,15 @@
 def unsafe?(speed)
-
+  if speed < 40 || speed > 60 == true
+    true
+  else
+    false
+  end
 end
 
 
 
 def not_safe?(speed)
-	
+	(60 < speed) || (speed < 40) ? true : false
 end
 	
 
