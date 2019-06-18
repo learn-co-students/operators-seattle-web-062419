12ffcf6b0552cbf58e6b302d2bfc074f0f2ba38c
Grace Colette <ilovegrace09@gmail.com>
2019-06-18 22:07:11 +0000

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,10 +1,15 @@
 def unsafe?(speed)
-
+  if speed == 50
+    return false
+  elsif speed > 60
+    return true
+  else 
+    return true
+end
 end
 
-
-
 def not_safe?(speed)
+  speed == 50 ? false : true
 	
 end
 	
